b330a0b650e0f53e66685a85037113ebac84709e
andrew <aj.gray619@gmail.com>
2021-04-18 15:24:20 -0700

    removeTestAndBuildFromPreCommit

--- scripts/preCommit.sh
+++ scripts/preCommit.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 ./scripts/preCommit/protectDevAndMainBranches.sh || exit "$?"
-yarn build || exit "$?"
-#yarn test || exit "$?"  # SKIPPING TESTS FOR NOW AS THEY DON'T PASS FOR SOME REASON?
