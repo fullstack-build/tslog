1929006f49f9afd8255fc047a7a432a7468de179
Mark-Oliver Junge <root@nyrox.dev>
2023-02-09 15:17:54 +0100

    Actually export my types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { BaseLogger, ISettingsParam, ILogObj, ISettings } from "./BaseLogger.js";
 export { ISettingsParam, BaseLogger, ILogObj };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-declare class Logger<LogObj> extends BaseLogger<LogObj> {
+
+export declare class Logger<LogObj> extends BaseLogger<LogObj> {
   constructor(settings?: ISettings<LogObj>, logObj?: LogObj);
 }
