306d4e1d06f28b5e28924a825df631937baf8bf0
Eugene Terehov <opensource@terehov.de>
2023-08-07 22:10:24 +0200

    Prettier

--- examples/sveltekit_vite/vite-example/src/main.ts
+++ examples/sveltekit_vite/vite-example/src/main.ts
@@ -1,13 +1,12 @@
-import './app.css'
-import App from './App.svelte'
+import "./app.css";
+import App from "./App.svelte";
 
 import { Logger } from "../../../../";
 const log: Logger<any> = new Logger();
 log.silly("I am a silly log.");
 
-
 const app = new App({
-  target: document.getElementById('app'),
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
